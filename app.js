bf45967b37ada465ff269750831e7d770333d969
Pedro Ramos <pedroramos327698@gmail.com>
2022-03-29 14:44:32 -0300

    starting-project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path')
+const app = express()
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is runing at ${PORT}`)
+})
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// rotas
+app.use('/', require('./routes/routes'))
---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const res = require('express/lib/response');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.send('deu certo');
+});
+
+router.get('/', (req, res) => {
+  res.render('content')
+})
+
+module.exports = router
